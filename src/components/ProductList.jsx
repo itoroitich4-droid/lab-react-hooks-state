39294e80a3715add69ec402eeec3e2ce5d7a83b5
itoroitich4-droid <itoroitich4@gmail.com>
2026-08-07 14:17:51 +0300

    k

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -13,13 +13,14 @@ const ProductList = ({ products, category, addToCart }) => {
   return (
     <div>
       <h2>Available Products</h2>
-      {filteredProducts.map((product) => (
-        <ProductCard 
-          key={product.id} 
-          product={product} 
-          addToCart={addToCart} 
-        />
-      ))}
+      {filteredProducts.length === 0 ? (
+  <p>No products available</p>
+) : (filteredProducts.map((product) => (<ProductCard     key={product.id}
+      product={product}
+      addToCart={addToCart}
+    />
+  ))
+)}
     </div>
   )
 }
